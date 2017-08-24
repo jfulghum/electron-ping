0203bda070a93ae78034513cc4245ca1edc3b711
Johanna Fulghum <johanna.fulghum@gmail.com>
2017-08-23 20:56:09 -0400

    purple icon

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const {app, Menu, Tray} = require('electron')
 
 let tray = null
 app.on('ready', () => {
-  tray = new Tray('/Users/johannafulghum/Desktop/pic.png')
+  tray = new Tray('tray_icon_black.png')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Item1', type: 'radio'},
     {label: 'Item2', type: 'radio'},
     {label: 'Item3', type: 'radio', checked: true},
     {label: 'Item4', type: 'radio'},
     {label: 'Quit!', role: 'quit'},
-    {label: 'Clickable', click(){console.log("We clicked!")}}
-
+    {label: 'Clickable', click(){console.log("We clicked!")}},
+    {label: 'Purple', click(){tray.setImage("tray_icon_purple.png")}}
 
   ])
+
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
 
